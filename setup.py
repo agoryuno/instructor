a03f83a440fd664e32a723bff7b329ba04ff049b
Alexander Goryunov <43528668+agoryuno@users.noreply.github.com>
2023-01-03 00:37:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='instructor',
+    version='0.1',
+    packages=['instructor'],
+    url='',
+    license='',
+    author='',
+    description='',
+    requires_python='>=3.8',
+    install_requires=[
+        'datasets',
+        'evaluate',
+        'scikit-learn',
+        'torch',
+        'transformers',
+        'wandb'
+    ]
+)
