9399a5652bfc08109030ba5c093205400ba926dc
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-03 03:00:51 +0200

    /.

--- instructor/trainer.py
+++ instructor/trainer.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 import os
 from argparse import ArgumentParser
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Union
 
 import evaluate
 import numpy as np
@@ -29,11 +28,6 @@ parser = ArgumentParser()
 parser.add_argument("config", type=str)
 
 
-@dataclass
-class CustomTrainingArguments(TrainingArguments):
-    loss_function: str = "rank"
-
-
 def compute_metrics(eval_pred):
     predictions, _ = eval_pred
     predictions = np.argmax(predictions, axis=1)
@@ -51,35 +45,13 @@ class RankLoss(nn.Module):
 
 
 class RankTrainer(Trainer):
-    def __init__(
-        self,
-        model: Union[PreTrainedModel, nn.Module] = None,
-        args: TrainingArguments = None,
-        data_collator: Optional[DataCollator] = None,
-        train_dataset: Optional[Dataset] = None,
-        eval_dataset: Optional[Dataset] = None,
-        tokenizer: Optional[PreTrainedTokenizerBase] = None,
-        model_init: Callable[[], PreTrainedModel] = None,
-        compute_metrics: Optional[Callable[[EvalPrediction], Dict]] = None,
-        callbacks: Optional[List[TrainerCallback]] = None,
-        optimizers: Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LambdaLR] = (None, None),
-        preprocess_logits_for_metrics: Callable[[torch.Tensor, torch.Tensor], torch.Tensor] = None,
-    ):
-        super().__init__(
-            model,
-            args,
-            data_collator,
-            train_dataset,
-            eval_dataset,
-            tokenizer,
-            model_init,
-            compute_metrics,
-            callbacks,
-            optimizers,
-            preprocess_logits_for_metrics,
-        )
-        self.loss_fct = RankLoss() if args.loss_function == "rank" else nn.CrossEntropyLoss()
-        self.loss_function = args.loss_function
+    def __init__(self, loss_function: Optional[Literal["rank"]] = None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loss_fct = nn.CrossEntropyLoss()
+        self.loss_function = None
+        if args and loss_function == "rank":
+            self.loss_fct = RankLoss()
+            self.loss_function = loss_function
 
     def compute_loss(self, model, inputs, return_outputs=False):
         # forward pass
@@ -136,14 +108,14 @@ if __name__ == "__main__":
         print("Number of trainable : {}M".format(int(params / 1e6)))
 
     tokenizer = get_tokenizer(model_name)
-    args = CustomTrainingArguments(
+    args = TrainingArguments(
         output_dir=f"{model_name}-finetuned",
         num_train_epochs=training_conf["num_train_epochs"],
         warmup_steps=500,
-        loss_function=training_conf["loss"],
         learning_rate=training_conf["learning_rate"],
         # half_precision_backend="apex",
-        fp16=True,
+        #fp16=True,
+        fp16=False,
         gradient_checkpointing=training_conf["gradient_checkpointing"],
         gradient_accumulation_steps=training_conf["gradient_accumulation_steps"],
         per_device_train_batch_size=training_conf["per_device_train_batch_size"],
@@ -177,10 +149,11 @@ if __name__ == "__main__":
     trainer = RankTrainer(
         model,
         args,
+        loss_function=training_conf["loss"],
         train_dataset=train,
         eval_dataset=eval,
         data_collator=collate_fn,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics,
     )
-    trainer.train()
+    #trainer.train()
