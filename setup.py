ea814044f1e8cd3ac3034a7db436a89463b4d272
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-03 12:49:52 +0200

    Fixing requirements

--- setup.py
+++ setup.py
@@ -2,19 +2,11 @@ from setuptools import setup
 
 setup(
     name='instructor',
-    version='0.1',
+    version='0.2',
     packages=['instructor'],
     url='',
     license='',
     author='',
     description='',
-    requires_python='>=3.6',
-    install_requires=[
-        'datasets',
-        'evaluate',
-        'scikit-learn',
-        'torch',
-        'transformers',
-        'wandb'
-    ]
+    requires_python='>=3.6'
 )
