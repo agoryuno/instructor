d5d66425491a55d758cf5c39e690b67dfc4008eb
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-03 13:27:49 +0200

    Typo in the torch dep

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "datasets==2.8.0",
         "evaluate==0.4.0",
         "scikit-learn==1.2.0",
-        "torch==1.12.1+cu116",
+        "torch==1.12.1+cpu116",
         "transformers==4.25.1",
         "wandb==0.13.7",
         "protobuf==3.20.*"
