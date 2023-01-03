f8c8981db7bb1de56cc9962469b8b850b95bcebf
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-03 12:55:38 +0200

    Requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ from setuptools import setup
 
 setup(
     name='instructor',
-    version='0.2',
+    version='0.2.0.1',
     packages=['instructor'],
     url='',
     license='',
     author='',
     description='',
-    requires_python='>=3.6'
-)
+    requires_python='>=3.6',
+    install_requires=[
+        "datasets==2.8.0",
+        "evaluate==0.4.0",
+        "scikit-learn==1.2.0",
+        "torch==1.12.1+cu116",
+        "transformers==4.25.1",
+        "wandb==0.13.7",
+        "protobuf==3.20.*"
+        ]
+    )
